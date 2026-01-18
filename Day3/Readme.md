8e75b22a7effb8d10d846054d97b36864f2d7386
aryan-098max <aryangupta4279@gmail.com>
2026-01-18 19:01:14 +0530

    Day 3

--- Day3/Readme.md
+++ Day3/Readme.md
@@ -4,7 +4,11 @@
 
 1. First Iterate through the entire array
 2. Find the first largest element 
-3. Use that first largest as means to find the second largest
+3. Before assigning a new fLargest, re-assign the sLargest with fLargest
+4. Always check that arr[i]!=fLargest
+5. All element same - return sLargest === -Infinity ? null:sLargest
+6. The code you write should pass the all the test cases.
+
 
 # Complexity
 
--- Day3/secondLargest.js
+++ Day3/secondLargest.js
@@ -1,26 +1,26 @@
 function secondLargest(arr){
 
-    let fLargest = arr[0];
-    let sLargest = arr[1];
-
-    // swap elements
-    if(fLargest<sLargest){
-        [fLargest, sLargest] = [sLargest, fLargest];
+    if(arr.length < 2){
+        return null
     }
 
+    let fLargest = -Infinity;
+    let sLargest = -Infinity;
+
     // fnding first largest & second largest
-    for(let i=2; i<arr.length;i++){
+    for(let i=0; i<arr.length;i++){
 
         if(arr[i]>fLargest){
+            sLargest = fLargest;
             fLargest = arr[i];
-        }
 
-        if(arr[i]>sLargest && arr[i]!=fLargest){
+        } else if (arr[i]>sLargest && arr[i]!=fLargest){
             sLargest = arr[i];
         }
     }
 
-    return sLargest;
+    // final check for all same element inside an array [5,5,5]
+    return sLargest === -Infinity? null:sLargest;
     
 }
 const arr1 = [9,1,2,4,5,8];
