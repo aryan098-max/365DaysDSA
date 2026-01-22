b608dabc1f0c3ade96d18b3a64437f7c4bbdbb4e
aryan-098max <aryangupta4279@gmail.com>
2026-01-22 23:07:15 +0530

    Day 8

--- Day8/alternateNumberPattern.js
+++ Day8/alternateNumberPattern.js
@@ -9,6 +9,7 @@
 
 */
 
+// 1st Way
 function alternateNumberPattern(n){
 
     for(let i=0; i<n;i++){
@@ -29,5 +30,33 @@ function alternateNumberPattern(n){
 
 }
 
+// 2nd Way
+function switchNumberPattern(n){
+
+    for(let i=0;i<n;i++){
+
+        let row = "";
+        let toggle = 1;
+
+        for(let j=0; j<i+1; j++){
+
+            row = row + toggle;
+
+            if(toggle === 1){
+                // switch the value of toggle
+                toggle = 0;
+            } else {
+                // switch again
+                toggle = 1;
+            }
+        }
+
+        console.log(row);
+    }
+
+
+}
+
 let n = 6;
 alternateNumberPattern(n);
+switchNumberPattern(n);
