66eb9bce16b27420863406b516bb81e849711a9e
Aryan Gupta <aryangupta@192.168.1.6>
2026-01-24 15:07:23 +0530

    Day 10

---
+++ Day10/Readme.md
@@ -0,0 +1,14 @@
+# Question
+reverse Integer covering negative logic as well
+
+# Problem Pattern
+1. Reverse an Integer using mathematically approach (n%10) & (n/10);
+2. if(num<) return reversedNum with prefix (-reversedNum)
+
+# Complexity
+
+1. Time Complexity
+- O(n)
+
+2. Space Complexity
+- O(1)
--- Day10/reverseInteger.js
+++ Day10/reverseInteger.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+function reverseInteger(num){
+
+    let numCopy = num;
+    numCopy = Math.abs(numCopy);
+    let reversedNum = 0;
+
+    while(numCopy>0){
+
+        let lastDigit = numCopy%10;
+        reversedNum = (reversedNum * 10) + lastDigit;
+        numCopy = Math.floor(numCopy/10);
+    }
+
+    return (num<0) ? -reversedNum : reversedNum
+}
+
+let num = -123;
+const reversedNumber = reverseInteger(num);
+console.log(reversedNumber);
