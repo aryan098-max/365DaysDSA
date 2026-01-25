6f6969e3dcb8e8afd4efe1de1ee2166c3e9f2a8f
aryan-098max <aryangupta4279@gmail.com>
2026-01-25 21:03:01 +0530

    Day 11

--- Day11/Complexity.js
+++ Day11/Complexity.js
@@ -2,4 +2,21 @@
   - Linear Search - n times
   - Binary Search - logn times
 
+  O(1) > O(log n) > O(n) > O(n logn) > O(2^n) > O(n!)
+
+  - Binary Search is an example of O(log n) as the problem is being divided by 2 
+  every time
+
+  n = 10
+
+  n/2^x = 1 
+
+  Taking log both sides 
+  log n = log 2^x
+
+  According to log, log 2 = 1
+  log n = x 
+
+  Similarly, we have O(n) Linear because the graph is straight line for this
+
  */
--- Day11/Readme.md
+++ Day11/Readme.md
@@ -1,1 +1,15 @@
 # Complexity Discusssion
+
+1. Time Complexity
+- O(1)
+- O(log n)
+- O(n)
+- O(n logn)
+- O(n^2)
+- O(n^3)
+- O(n!)
+
+2. Space Complexity
+- O(1) - constant - variable declaration
+- O(n) - An extra array space allocation
+- O(n^2) - 2D space allocation
