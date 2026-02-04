7ec01aa8f7ace911d1279f3dc7e6b98d85ad6e05
aryan-098max <aryangupta4279@gmail.com>
2026-02-04 09:59:25 +0530

    Day 21

--- Day20/singleNumber.js
+++ Day20/singleNumber.js
@@ -1,9 +1,15 @@
 function singleNumber(nums){
 
-    
+    let xor = 0;
 
+    for(let i=0; i<nums.length; i++){
+
+        xor = xor ^ nums[i];
+    }
+    
+    return xor;
 }
 
 const nums = [1,2,2,3,3,4,4];
 const output = singleNumber(nums);
-console.log(output);
+console.log(output); 
