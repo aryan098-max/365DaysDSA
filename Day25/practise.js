25a5ed87f277431d760eba584e85b9638dc3a7b1
Aryan Gupta <aryangupta4279@gmail.com>
2026-02-09 00:26:30 +0530

    Day 25

--- Day25/practise.js
+++ Day25/practise.js
@@ -1,18 +1,20 @@
-function removeDuplicates(nums){
+function removeElement(nums){
 
     let t = 0;
 
-    for(let i=1; i<nums.length; i++){
+    for(let i=0; i<nums.length; i++){
 
-        if(nums[i]>nums[t]){
+        if(nums[i]!==val){
+            nums[t] = nums[i];
             t = t+1;
-            nums[t] = nums[i]
         }
     }
 
-    return t+1;
+    return t;
+    
 }
 
-const nums = [1,1,2,2,3,];
-const k = removeDuplicates(nums);
+const nums = [3,2,2,3];
+const val = 3
+const k = removeElement(nums, val);
 console.log(k);
