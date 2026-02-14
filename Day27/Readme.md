edd14f2d2237af51c1d5869892ff835aa5ac91d8
Aryan Gupta <aryangupta4279@gmail.com>
2026-02-14 23:41:52 +0530

    Day 31

---
+++ Day27/Readme.md
@@ -0,0 +1,17 @@
+# Problem - Linear Search
+
+- Input - [9,8,1,2,3], val = 9;
+- Output - 0, return index
+
+# Probelm Pattern
+
+- Traverse through the entire array and match the val for each element
+- Return the index after the element is found
+
+# Complexity
+
+# Time Complexity
+- O(n)
+
+# Space Complexity
+- O(1)
--- Day27/linearSearch.js
+++ Day27/linearSearch.js
@@ -1,9 +1,15 @@
 function linearSearch(nums, val){
 
+    for(let i=0; i<nums.length; i++){
 
+        if(nums[i] === val){
+            return i
+        }
+    }
 
 }
 
 const nums = [1,2,3,4,5];
-const val = 2
-const output = linearSearch(nums, val)
+const val = 4
+const output = linearSearch(nums, val);
+console.log(output);
