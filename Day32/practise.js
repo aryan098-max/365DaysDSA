63b9ae897671730c5f49771dd98492388dfd771b
Aryan Gupta <aryangupta4279@gmail.com>
2026-02-16 11:14:29 +0530

    Day 33

--- Day32/practise.js
+++ Day32/practise.js
@@ -1,14 +1,27 @@
-function isPowerOfTwo(n){
+function mergeSort(nums1, nums2, m, n){
 
-    if(n === 1) return true;
+    let t1 = m-1;
+    let t2 = n-1;
+    let x = nums1.length-1;
 
-    if(n<1 || n%2 !=0) return false;
+    for(let i=x; i>=0; i--){
 
-    return isPowerOfTwo(n/2);
+        if(t2<0 || t1>=0 && nums1[t1]>nums2[t2]){
 
+            nums1[i] = nums1[t1];
+            t1--;
+        } else {
+            nums1[i] = nums2[t2];
+            t2--;
+        }
+    }
 
+    return nums1;
 }
 
-const n = 16
-const output = isPowerOfTwo(n);
+const nums1 = [1,2,3,0,0,0];
+const nums2 = [2,5,6];
+const m = 3;
+const n = 3;
+const output = mergeSort(nums1, nums2, m, n);
 console.log(output);
