df625c67f27f504e088a8c2e5c3a858c51289ffd
Aryan Gupta <aryangupta4279@gmail.com>
2026-02-17 06:32:55 +0530

    Day 34

---
+++ Day34/practise.js
@@ -0,0 +1,27 @@
+function bubbleSort(nums){
+
+    n = nums.length;
+
+    for(let i=0; i<n-1; i++){
+
+        let isSwapped = false;
+
+        for(let j=0; j<n-i-1; j++){
+
+            if(nums[j]>nums[j+1]){
+                let temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
+                isSwapped = true
+            }
+            if(!isSwapped) break 
+        }
+
+    }
+
+    return nums;
+}
+
+const nums = [5,2,4,1];
+const output = bubbleSort(nums);
+console.log(output);
