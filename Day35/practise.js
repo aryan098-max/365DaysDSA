317f424d76dc76a6707ae15cc0dbf49d99d8f983
aryan-098max <aryangupta4279@gmail.com>
2026-02-18 08:59:08 +0530

    Day 35

--- Day35/practise.js
+++ Day35/practise.js
@@ -1,23 +1,11 @@
-function fibIterative(n){
+function fibRecursive(n){
 
     // base case
     if(n<=1) return n;
 
-    let preValue = 0;
-    let currentValue = 1;
-    let nextValue = 0;
-
-    for(let i=1; i<n; i++){
-
-        nextValue = preValue + currentValue;
-        preValue = currentValue;
-        currentValue = nextValue;
-
-    }
-
-    return nextValue;
+    return fibRecursive(n-1) + fibRecursive(n-2);
 }
 
 const n = 5;
-const output = fibIterative(n);
+const output = fibRecursive(n);
 console.log(output);
