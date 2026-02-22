48ac83408883ba528793ea498c789af10281f30c
Aryan Gupta <aryangupta4279@gmail.com>
2026-02-22 20:13:35 +0530

    Day 39

---
+++ Day37/balancedStringSplit2.js
@@ -0,0 +1,24 @@
+function balancedStringSplit(s){
+
+    let temp = 0;
+    let count = 0;
+
+    for(let i=0; i<s.length; i++){
+
+        if(s[i] === 'R'){
+            temp++;
+        } else{
+            temp--;
+        }
+
+        if(temp === 0){
+            ++count;
+        }
+    }
+
+    return count;
+}
+
+const s = "RLRLRLRRRLLL"
+const output = balancedStringSplit(s);
+console.log(output);
