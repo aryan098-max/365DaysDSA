f2cae5a74a4416d1e5aa4b684696afaa50d7258d
aryan-098max <aryangupta4279@gmail.com>
2026-03-03 15:09:32 +0530

    Day 48

---
+++ Day45/linkedList.js
@@ -0,0 +1,41 @@
+/**
+ # Linked List - Data Structure
+
+- It is a linear data structure. Nodes are linked together by a reference field.
+
+- Two types of Linked Lists:-
+1. Singly Linked List
+2. Doubly Linked List
+
+- It has head, from which we can traverse to other nodes. We have pointer to the head. 
+- The last node is pointing to null.
+
+1. Single Linked List - It is pointing towards another list. It is not pointing back to
+   the previous node.
+
+2. Doubly Linked List - It is pointing back and forth. It is pointing towards the forward 
+   node and backward node at the same time.
+
+# Differnce between Array and Linked List
+
+1. Array Points
+
+- Linear - Stored on indexes
+- Contigious - They stored one after another
+- Fixed Size (Can be Dynamic)
+- Just Value - only values are stored
+- Getting element is easy - arr[2]
+- Insertion/Deletion is complex
+- Memory Efficient
+
+
+2. Linked List
+
+- Linear - pointer
+- Non-contigious
+- Dynamic (change easily)
+- Node - (value + pointers)
+- Getting element is hard - You can't directly jump to values, you must travese the List
+- Insertion & Deletion is easy
+- Extra Memory
+ */
