d9a807d5753846c08f31e1f3e6535ab44c302fa5
Aryan Gupta <aryangupta4279@gmail.com>
2026-03-20 17:21:15 +0530

    Day 65

---
+++ Day55/practise.js
@@ -0,0 +1,28 @@
+var lengthOfLastWord = function(s) {
+
+    let i = s.length-1;
+    let lastWordCount=0;
+
+    while(i>=0){
+
+        if(s[i] !== " "){
+            break;
+        }
+        i--;
+    }
+
+    while(i>=0){
+
+        if(s[i] === " "){
+            break;
+        }
+        lastWordCount ++;
+        i--;
+    }
+    
+    return lastWordCount;
+};
+
+const s = "a";
+const output = lengthOfLastWord(s);
+console.log(output);
