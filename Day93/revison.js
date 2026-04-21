0d4fdd277b9e3992713a75f029fb670817a71d3c
Aryan Gupta <aryangupta4279@gmail.com>
2026-04-21 18:48:50 +0530

    Day 93

--- Day93/revison.js
+++ Day93/revison.js
@@ -1,10 +1,28 @@
-function selectionSort(nums){
+function insertionSort(nums){
 
     let n = nums.length;
 
+    // First loop starts from i=1, cause i=0 is the prev
+    for(let i=1; i<nums.length; i++){
+
+        // Two variables
+        let curr = nums[i];
+        let prev = i-1;
+
+        while(nums[prev] > curr && prev >=0){
+
+            // keep moving prev forward and keep reducing the prev for further elements
+            nums[prev + 1] = nums[prev];
+            prev --;
+        }
+
+        // Next, after moving all the prev element forward move curr to the prev-1
+        nums[prev + 1] = curr;
+    }
+    return nums;
    
 }
 
 const nums = [5,1,0,9,8,7,6];
-const output = selectionSort(nums);
+const output = insertionSort(nums);
 console.log(output);
