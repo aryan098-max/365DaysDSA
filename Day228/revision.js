3780342a8cb3832b42583d8b3c337b3f43a9a26f
Aryan Gupta <aryangupta4279@gmail.com>
2026-09-01 23:12:54 +0530

    Day 228

---
+++ Day228/revision.js
@@ -0,0 +1,32 @@
+var isValid = function(s) {
+
+
+    // base case || corner case
+    if(s.length <= 1){
+        return false;
+    }
+
+    let stack = [];
+     let bracketMap = {
+        "[":"]",
+        "{":"}",
+        "(":")"
+     }
+
+    for(let i=0; i<s.length; i++){
+
+        // how to check opening or close bracket
+        if(bracketMap[s[i]]){
+            stack.push(s[i]);
+        } 
+        else {
+            // pop the last value from the stock
+            let top = stack.pop();
+            if(!top || s[i] !== bracketMap[top]){
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+    
+};
