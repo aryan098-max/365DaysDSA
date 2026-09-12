95bad7ce0c5daca1aeaddc0787b10182bf446231
Aryan Gupta <aryangupta4279@gmail.com>
2026-09-12 23:48:14 +0530

    Updates

---
+++ Day173/revision.js
@@ -0,0 +1,20 @@
+var middleNode = function(head) {
+
+      // We have to use slow and fast pointer to solve this problem
+
+    let slow = head
+    let fast = head;
+
+    // I have to use and for the case if fast is already null
+    while(fast !== null && fast.next !== null){
+
+        // moving slow ones
+        slow = slow.next;
+
+        // moving fast two times
+        fast = fast.next.next;
+    }
+    
+    return slow;
+    
+};
