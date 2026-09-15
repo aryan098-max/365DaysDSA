f8e721e2ba8624d51e65c4fae1f4c75a6094f19c
Aryan Gupta <aryangupta4279@gmail.com>
2026-09-15 23:21:48 +0530

    Day 241

---
+++ Day241/revision.js
@@ -0,0 +1,22 @@
+// # 485. Max Consecutive Ones
+
+var findMaxConsecutiveOnes = function(nums) {
+    
+     let maxCount = 0;
+    let currentCount = 0;
+
+    for(let i=0; i<nums.length; i++){
+
+        if(nums[i] === 1){
+            currentCount ++;
+        } else{
+            currentCount = 0;
+        }
+
+        if(maxCount < currentCount){
+            maxCount = currentCount;
+        }
+    }
+    
+    return maxCount;
+};
