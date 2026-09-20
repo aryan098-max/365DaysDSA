59999a6b5435127651b9f36f83abfc3860e71852
Aryan Gupta <aryangupta4279@gmail.com>
2026-09-20 23:45:43 +0530

    Day 246

---
+++ Day246/revision.js
@@ -0,0 +1,30 @@
+// # 69. Sqrt(x)
+
+var mySqrt = function(x) {
+
+    // base for 0 and 1
+    if(x<2){
+        return x;
+    }
+
+    let left = 2
+    let right = Math.floor(x/2);
+    
+    while(left <= right){
+
+        let middle = Math.floor((left + right)/2);
+        
+        if(x === middle ** 2){
+            return middle;
+        }
+        else if(x < middle ** 2){
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+    }
+    // If a given number is not a perfect square, right exceeds left
+    // right is our ans
+    return right;
+};
