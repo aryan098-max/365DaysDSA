292abae2df2ba68042c1e30526d42ad11a9eaa87
Aryan Gupta <aryangupta4279@gmail.com>
2026-09-24 22:21:45 +0530

    Day 249

---
+++ Day249/revision.js
@@ -0,0 +1,16 @@
+// # 9. Palindrome Number
+
+var swapPairs = function(head) {
+    
+    if(head === null || head.next === null){
+        return head; // head is null at the last recursion
+    }
+
+    let left = head;
+    let right = head.next;
+
+    left.next = swapPairs(right.next);
+    right.next = left;
+    return right;
+};
+
